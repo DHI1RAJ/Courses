e910c5941f7f2eb62d6b62bea412373debccf889
Dhiraj <singhdhiraj461@gmail.com>
2024-08-11 02:07:39 +0530

    Added API for deleteCourse and updateCourse

--- src/main/java/com/Rest/Courses/Courses/controller/myController.java
+++ src/main/java/com/Rest/Courses/Courses/controller/myController.java
@@ -3,6 +3,8 @@ package com.Rest.Courses.Courses.controller;
 import com.Rest.Courses.Courses.Entities.Course;
 import com.Rest.Courses.Courses.service.courseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,5 +32,19 @@ public class myController {
     @PostMapping("/courses")
     public Course addCourse(@RequestBody Course course){
         return this.courseService.addCourse(course);
+    }
+    @DeleteMapping("/courses/{courseId}")
+    public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId){
+        try{
+            this.courseService.deleteCourse(Long.parseLong(courseId));
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PutMapping("/course")
+    public Course updateCourse(@RequestBody Course course){
+        return this.courseService.updateCourse(course);
     }
 }
